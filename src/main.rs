f9e92a623300dfcc15260a2b15ea5b44cf72c1f0
Yossi Kreinin <Yossi.Kreinin@gmail.com>
2024-03-13 22:37:49 +0200

    only replacing in the .rodata*, .debug_line* and .debug_str* sections

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,77 @@
 
-use goblin::elf::{header::*, section_header::*, Elf};
+use goblin::elf::Elf;
 use memmap2::Mmap;
 use std::env;
 use std::fs::File;
 use std::io::{self, Write};
 use std::process;
-use procinfo::pid::{statm_self, Statm};
 
-fn memuse() {
-    match statm_self() {
-        Ok(statm) => {
-            println!("Memory used by the process: {} KB", statm.size);
-        }
-        Err(e) => {
-            eprintln!("Error: {}", e);
-        }
-    }
-}
-
-fn replace_bytes(data: &mut [u8], src: &[u8], dest: &[u8]) {
+fn replace_bytes(data: &mut [u8], src: &[u8], dst: &[u8]) {
     let src_len = src.len();
-    let dest_len = dest.len();
+    let dst_len = dst.len();
 
     for i in 0..data.len() - src_len + 1 {
         if &data[i..i + src_len] == src {
-            data[i..i + dest_len].copy_from_slice(dest);
+            data[i..i + dst_len].copy_from_slice(dst);
         }
     }
 }
-
-/*
-fn replace_bytes(data: &mut [u8], src: &[u8], dest: &[u8]) {
-    for chunk in data.windows(src.len()) {
-        if chunk == src {
-            chunk.copy_from_slice(dest);
-        }
-    }
-}
-*/
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
-        writeln!(io::stderr(), "Usage: {} <elf_file> <src> <dst>", args[0]).unwrap();
+        writeln!(io::stdout(), "Usage: {} <elf_file> <src> <dst>", args[0]).unwrap();
         process::exit(1);
     }
 
     let elf_file_path = &args[1];
     let src_bytes = args[2].as_bytes();
-    let dest_bytes = args[3].as_bytes();
+    let dst_bytes = args[3].as_bytes();
+
+    if src_bytes.len() != dst_bytes.len() {
+        writeln!(io::stdout(), "source and destination strings should have the same length in bytes, instead `{}' has {} bytes but `{}' has {} bytes",
+                 args[2], src_bytes.len(), args[3], dst_bytes.len()).unwrap();
+	process::exit(1);
+    }
 
     let elf_file = match File::options().read(true).write(true).open(elf_file_path) {
         Ok(f) => f,
         Err(e) => {
-            writeln!(io::stderr(), "Error opening file: {}", e).unwrap();
+            writeln!(io::stdout(), "Error opening file `{elf_file_path}': {e}").unwrap();
             process::exit(1);
         }
     };
 
-    memuse();
     let mmap = unsafe { Mmap::map(&elf_file).unwrap() };
-    memuse();
 
+    let prefix_list = vec![
+        ".rodata",
+        ".debug_line",
+        ".debug_str",
+    ];
+    
+    let mut sections = Vec::new(); 
     match Elf::parse(&mmap) {
         Ok(elf) => {
-            memuse();
-            println!("Section headers:");
-            for header in elf.section_headers.iter() {
+    	    for header in elf.section_headers.iter() {
                 let name = elf.shdr_strtab.get_at(header.sh_name).unwrap_or("<invalid utf-8>");
-                println!(
-                    "Name: {:?}, Address: {:#x}, Size: {:#x}",
-                    name,
-                    header.sh_addr,
-                    header.sh_size
-                );
-            }
-        }
+                let name_matches = prefix_list.iter().any(|prefix| name.starts_with(prefix));
+                if name_matches {
+	            sections.push(header.clone());
+                }
+	    }
+	},
         Err(e) => {
-            writeln!(io::stderr(), "Error parsing ELF file: {}", e).unwrap();
+            writeln!(io::stdout(), "Error parsing ELF file `{elf_file_path}': {e}").unwrap();
             process::exit(1);
         }
-    }
+    };
 
-    replace_bytes(&mut mmap.make_mut().unwrap(), src_bytes, dest_bytes);
-    //mmap.make_mut().unwrap().flush_async();
+    let mut mmap_mut = mmap.make_mut().unwrap();
+    for header in sections {
+        let offset = header.sh_offset as usize;
+        let size = header.sh_size as usize;
+        replace_bytes(&mut mmap_mut[offset .. offset+size], src_bytes, dst_bytes);
+    }
 }
 
